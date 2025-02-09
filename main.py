49e3a8a9c1fae64e314c997d17bf463ef9dd2917
undaunted43 <yilmaz.a.kont@gmail.com>
2025-02-09 14:17:45 +0300

    second commit after circleshape class

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock =  pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        value_ = clock.tick(60)
+        dt = value_/1000
 
 if __name__ == "__main__":
     main()
